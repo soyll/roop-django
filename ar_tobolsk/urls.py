e800f63162023b5ae4f1cd71c971ca4cb38ba627
soyll <soyll@vk.com>
2025-07-21 23:01:37 +0300

    fix: update routes

--- ar_tobolsk/urls.py
+++ ar_tobolsk/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('api/', include('faceswap.urls')),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
