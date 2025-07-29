e1d62530044d066d2712c621d2fb888c9d68a0c5
soyll <soyll@vk.com>
2025-07-30 01:03:27 +0300

    hotfix: import swagger

--- ar_tobolsk/settings.py
+++ ar_tobolsk/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_spectacular',
     'rest_framework',
     'rest_framework.authtoken',
     'faceswap',
